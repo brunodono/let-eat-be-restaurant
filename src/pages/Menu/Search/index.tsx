d7d6d20212701e18bfd1d4112ece0b2b77709f42
brunodono <bruno_bignard@hotmail.com>
2022-08-03 18:00:03 +0100

    Added Menu with Search input

---
+++ src/pages/Menu/Search/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from './Search.module.scss';
+import { CgSearch } from 'react-icons/cg'
+
+interface Props {
+    search: string,
+    setSearch: React.Dispatch<React.SetStateAction<string>>
+}
+
+const Search = ({search, setSearch}: Props) => {
+    return(
+<div className={styles.search}>
+    <input
+    value={search}
+    onChange={event => setSearch(event.target.value)}
+    />
+    <CgSearch size={25} color="#4C4D5E" />
+</div>
+    )
+}
+
+export default Search
--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,12 +1,25 @@
+
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo } from 'assets/logo.svg'
+import Search from './Search';
+import { useState } from 'react';
 
 const Menu = () => {
-    return(
+    const [search, setSearch] = useState("")
+    return (
         <main>
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    Let the pasta be your path
+                </div>
+            </header>
+            <section className={styles.menu}>
+                <h3 className={styles.menu__title}>Menu</h3>
+                <Search search={search} setSearch={setSearch} />
+            </section>
         </main>
     )
 
