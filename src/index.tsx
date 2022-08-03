38afe2532df8fb0f402cd034220c16c384382404
brunodono <bruno_bignard@hotmail.com>
2022-08-03 14:46:04 +0100

    Added css reset, svgr reader and absolute import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Menu from './pages/Menu';
 
 
 const root = ReactDOM.createRoot(
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div> Let eat be </div>
+    <Menu />
   </React.StrictMode>
 );
