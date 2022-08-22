620d78464dbfead0e5e0cc54f6f1ef9b098aaeed
brunodono <bruno_bignard@hotmail.com>
2022-08-22 13:23:23 +0100

    Created NotFound route

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,16 @@
+import Footer from 'components/Footer';
 import Nav from 'components/Nav';
 import StandardPage from 'components/StandardPage';
 import About from 'pages/About';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
+import NotFound from 'pages/NotFound';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 const AppRouter = () => {
     return (
-        <main>
+        <main className='container'>
             <Router>
                 <Nav />
                 <Routes>
@@ -17,7 +19,9 @@ const AppRouter = () => {
                         <Route path='menu' element={<Menu />} />
                         <Route path='about' element={<About />} />
                     </Route>
+                    <Route path='*' element={<NotFound />} />
                 </Routes>
+                <Footer />
             </Router>
         </main>
 
